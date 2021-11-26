ec9b8e6b4d9253df067a224fc7cf775282dcde1a
Henzii <80477931+Henzii@users.noreply.github.com>
2021-11-26 12:30:01 +0200

    Cypress nav test

--- cypress/integration/simpleTest.js
+++ cypress/integration/simpleTest.js
@@ -5,3 +5,11 @@ describe('Pokedex', function () {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Navtest', function() {
+  it ('navigate from front page to pickzxkckzatchu or whatever', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('pikachu').click()
+    cy.contains('lightning rod')
+  })
+})
