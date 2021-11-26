d5bec4852eebda45080108a9472b707e74c464ed
Henzii <80477931+Henzii@users.noreply.github.com>
2021-11-26 13:11:26 +0200

    Rollback pipeline & Broken /health endpoint

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (_req, res) => {
-  res.send('yläpeukku')
+  res.send('alapeukku')
 })
 app.get('/version', (_req, res) => {
   res.send('1')
